d58b610ee0f91cfeec5cbf4d1e02933f6f96260c
Akbar Lintang Aji <lintangajiakbar@gmail.com>
2021-02-08 22:34:34 +0700

    Finishing landing page design

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+if ($(window).width() > 992) {
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 40) {
+            $('#navbar-top').addClass("fixed-top");
+            $('body').css('padding-top', $('.top-nav').outerHeight() + 'px');
+        } else {
+            $('#navbar-top').removeClass("fixed-top");
+            $('body').css('padding-top', '0');
+        }
+    });
+}
